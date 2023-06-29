98f2e5467f8906727d808103f7762a18377d70b8
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2023-06-29 11:16:23 -0600

    Solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (!(num >= 0)) {
+    return 'ERROR'
+  }
 
-};
+  let repeatedString = ''
+
+  for (let index = 0; index < num; index++) {
+    repeatedString += string
+  }
+
+  return repeatedString
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
