42e29ba6cb962150c9b4e3a8dc286ca9363b8dde
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2023-07-05 23:18:06 -0600

    Start 07 with exactly one decimal returned

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  let tempInCelcius = (tempInFahrenheit - 32) * (5 / 9);
+  let roundedTempInCelcius = tempInCelcius.toFixed(1);
+  return roundedTempInCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelcius) {
+  let tempInFahrenheit = tempInCelcius * (9 / 5) + 32;
+  let roundedTempInFahrenheit = tempInFahrenheit.toFixed(1);
+  return roundedTempInFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
