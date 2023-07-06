02fc4cc811ebf12781e471fc4be696445256f4c5
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2023-07-05 22:20:16 -0600

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  if (firstNumber < 0 || secondNumber < 0) {
+    return 'ERROR';
+  }
 
+  // check types (lol TypeScript)
+  if (typeof secondNumber != 'number' || typeof firstNumber != 'number') {
+    return 'ERROR';
+  }
+
+  // declare sum, which will hold the sum as it is added up
+  let sum = 0;
+  let smallerNumber = 0;
+  let biggerNumber = 0;
+
+  // figure out biggerNumber for the loop
+  if (secondNumber > firstNumber) {
+    biggerNumber = secondNumber;
+    smallerNumber = firstNumber;
+  } else {
+    biggerNumber = firstNumber;
+    smallerNumber = secondNumber;
+  }
+
+  // add 'em up
+  for (
+    let currentNumber = biggerNumber;
+    currentNumber >= smallerNumber;
+    currentNumber--
+  ) {
+    sum += currentNumber;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
