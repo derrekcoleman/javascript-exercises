2a85e80d88106273bdaaee639c808df6c205d319
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2023-07-05 21:47:35 -0600

    Solve 03, 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (originalArray, ...itemsToRemoveArray) {
+  const trimmedArray = [];
 
+  for (let i = 0; i < originalArray.length; i++) {
+    if (originalArray[i] !== itemsToRemoveArray[0]) {
+      if (originalArray[i] !== itemsToRemoveArray[1]) {
+        if (originalArray[i] !== itemsToRemoveArray[2]) {
+          if (originalArray[i] !== itemsToRemoveArray[3]) {
+            trimmedArray.push(originalArray[i]);
+          }
+        }
+      }
+    }
+  }
+
+  return trimmedArray;
 };
 
 // Do not edit below this line
